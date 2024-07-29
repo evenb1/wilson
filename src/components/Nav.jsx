993368528cb0ceac8e4b596bb6bce64fc116cb94
Even <even.berhane2022@gmail.com>
2024-07-29 16:38:37 +0300

    nav links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,15 @@
 import{ headerLogo } from '../assets/images'
 const Nav = () => {
   return (
-<header className='padding-x '>
-        <nav>
+<header className='padding-x absolute z-10 w-full'>
+        <nav className='flex justify-between items-center max-container'>
             <a href="/">
                 <img src={headerLogo} alt='logo' width={100} height={30}></img>
             
             </a>
-            
+            <ul className='flex-1 flex justify-center items-center max-lg:hidden'>
+
+            </ul>
         </nav>
     </header>
       )
