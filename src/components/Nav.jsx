45a531b14d89432d732d83f6864319cafb6326e7
Even <even.berhane2022@gmail.com>
2024-07-29 17:14:38 +0300

    navlinks import

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,6 @@
 import{ headerLogo } from '../assets/images'
+import { hamburger } from '../assets/icons'
+import { navLinks } from '../constants'
 const Nav = () => {
   return (
 <header className='padding-x absolute z-10 w-full'>
@@ -8,7 +10,13 @@ const Nav = () => {
             
             </a>
             <ul className='flex-1 flex justify-center items-center max-lg:hidden'>
+                {navLinks.map((item) => (
+                    <li key={item.label}>
+                        <a href={item.href}>
 
+                        </a>
+                    </li>
+                ))}
             </ul>
         </nav>
     </header>
