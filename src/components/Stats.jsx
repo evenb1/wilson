a9dc4a6cb56e78c7d49af49f49fdfc2f3be6d031
Even <even.berhane2022@gmail.com>
2024-08-05 19:18:14 +0300

    stats countup

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+"use client";
+import CountUp from "react-countup";
+
+const stats = [
+  { num: 250, text: "Variations" },
+  { num: 500, text: "Shops" },
+  { num: 300, text: "Customers" }
+];
 
 const Stats = () => {
   return (
-    <div>Stats</div>
-  )
-}
+    <div className='max-container mx-auto'>
+      {stats.map((item, index) => (
+        <div key={index} className='text-center'>
+          <CountUp end={item.num} duration={5} delay={2} className="text-4xl xl:text-6xl font-extrabold" />
+          <p className="mt-2 text-lg">{item.text}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default Stats
+export default Stats;
