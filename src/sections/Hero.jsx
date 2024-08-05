42dc54d5c600448d7a09359ca5207be6c366ba42
Even <even.berhane2022@gmail.com>
2024-08-05 11:23:22 +0300

    hero headline and p tags

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,23 @@
 
 const Hero = () => {
   return (
-    <section id="home" className="w-full flex xl:flex-row flex-col  border-2 border-red-500 p-2">
-        
+    <section id="home" className="w-full flex xl:flex-row flex-col justify-center
+      min-h-screen gap-10 max-container">
+        <div className="relative xl:w-2/5 flex flex-col items-start w-full
+        max-xl:padding-x pt-28">
+            <h1>
+            BOLD NEW COLORS. 
+            <br/>
+            WHOLE NEW 
+            <span> BALLGAME.</span>
+            </h1>
+            <p>Welcome to Wilson Tennis, 
+                the industry leader in premium tennis equipment 
+                for over a century.</p>
+            
+
+
+        </div>
     </section>
 
 )
