66b4640618248c5980f1ebc3fcbd073697fd742d
Even <even.berhane2022@gmail.com>
2024-08-05 11:09:37 +0300

    hero section

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,32 +1,28 @@
-import{ headerLogo } from '../assets/images'
-import { hamburger } from '../assets/icons'
-import { navLinks } from '../constants'
+import { headerLogo } from '../assets/images';
+import { navLinks } from '../constants';
+import { hamburger } from '../assets/icons';
 const Nav = () => {
   return (
-<header className='padding-x absolute z-10 w-full'>
-        <nav className='flex justify-between items-center max-container'>
-            <a href="/">
-                <img src={headerLogo} alt='logo' width={100} height={30}></img>
-            
-            </a>
-            <ul className='flex-1 flex justify-end items-center max-lg:hidden gap-14'>
-                {navLinks.map((item) => (
-                    <li key={item.label}>
-                        <a href={item.href} className='font-montserrat leading-normal text-slate-gray'>
-                        {item.label}
-                        </a>
-                    </li>
-                ))}
-            </ul>
-            <div className=''>
-                <img src={hamburger}
-                alt='hambergur'
-                width={25}
-                height={25}></img>
-            </div>
-        </nav>
+    <header className='padding-x absolute z-10 w-full'>
+      <nav className='flex justify-between items-center max-container'>
+        <a href="/">
+          <img src={headerLogo} alt='logo' width={100} height={30} />
+        </a>
+        <ul className='flex-1 flex justify-end items-center max-lg:hidden gap-14'>
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a href={item.href} className='font-montserrat leading-normal text-slate-gray'>
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className='hidden max-lg:block'>
+          <img src={hamburger} alt='hamburger' width={25} height={25} />
+        </div>
+      </nav>
     </header>
-      )
+  );
 }
 
-export default Nav
+export default Nav;
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,11 @@
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
+    <section id="home" className="w-full flex xl:flex-row flex-col  border-2 border-red-500 p-2">
+        
+    </section>
+
+)
 }
 
 export default Hero
