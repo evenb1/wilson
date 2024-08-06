d2614d0b068ed473811e53ba80d76bc831d928ba
Even <even.berhane2022@gmail.com>
2024-08-06 14:36:10 +0300

    screen size position adjustments

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
   return (
     <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
       <div className="relative xl:w-2/5 flex flex-col items-start w-full max-xl:padding-x pl-10 pt-40">
-        <div className="text-4xl ml-0 mx-auto max-sm:text-[50px] text-black font-bold">
+        <div className="text-4xl ml-0 mx-auto max-sm:text-[50px]  text-black font-bold">
           BOLD NEW COLORS. <br />
           WHOLE NEW
           <FlipWords words={words} className="text-customcolor" />
@@ -26,11 +26,16 @@ const Hero = () => {
         <Stats />
       </div>
       <div
-        className="relative flex-1 flex justify-center items-center max-sm:hidden xl:min-h-screen"
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
+        className="relative flex-1 flex justify-center xl:m-0
+         lg:justify-end  min-lg:m-0 lg:mt-[-550px]  
+         items-center xl:min-h-screen md:justify-end md:mt-[-550px]"
+        
       >
-        <img src={isHovered ? bigImage3 : bigImage2} width={500} height={300} alt="" className="object-contain relative z-10" />
+        <img src={isHovered ? bigImage3 : bigImage2} alt="" 
+        className="object-contain w-[900px] h-[500px]
+         max-sm:h-[200px] relative lg:w-[700px] lg:h-[600px]
+          md:w-[400px] md:h-[300px] " onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}/>
       </div>
       
     </section>
