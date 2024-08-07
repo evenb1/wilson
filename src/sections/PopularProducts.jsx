c6a23f6144d6a749b73a697168a76396f97ef377
Even <even.berhane2022@gmail.com>
2024-08-07 16:20:01 +0300

    div styling for product cards

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -3,10 +3,14 @@ import { products } from "../constants"
 const PopularProducts = () => {
   return (
     <section id="products" className="max-container max-sm:mt-12">
-        <div className="felx flex-col">
+        <div className="felx flex-col justify-start gap-5 ">
             <h2 className="text-4xl font-palanquin font-bold"> 
             Our <span className="text-customcolor">Popular</span> Products</h2>
-            <p>Explore the best racket suited to your playing style</p>
+            <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">Explore the best racket suited to your playing style</p>
+        </div>
+        <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2
+        grid-cols-1 sm:gap-4 gap-14">
+
         </div>
     </section>
 )
