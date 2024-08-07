7639a6d8f7392b9475fc2ff5352a3fc06cf40c75
Even <even.berhane2022@gmail.com>
2024-08-07 16:07:43 +0300

    popular products header

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
   return (
     <main className='relative'>
       <Nav />
-      <section className='xl:padding-l wide:padding-r padding-b'>
+      <section className='xl:padding-l wide:padding-r '>
         <Hero />
       </section>
       <section className='padding'>
--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,8 +1,15 @@
+import { products } from "../constants"
 
 const PopularProducts = () => {
   return (
-    <div>PopularProducts</div>
-  )
+    <section id="products" className="max-container max-sm:mt-12">
+        <div className="felx flex-col">
+            <h2 className="text-4xl font-palanquin font-bold"> 
+            Our <span className="text-customcolor">Popular</span> Products</h2>
+            <p>Explore the best racket suited to your playing style</p>
+        </div>
+    </section>
+)
 }
 
 export default PopularProducts
