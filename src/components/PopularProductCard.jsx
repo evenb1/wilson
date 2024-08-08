f373d25b7e86447ef7c2d35b42a16179012a9d87
Even <even.berhane2022@gmail.com>
2024-08-08 12:18:56 +0300

    custom utility bg

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -12,7 +12,7 @@ const PopularProductCard = ({imgURL, imgURL2, name, price}) => {
         </img>
         <h3 className='mt-2 px-2 text-xl font-bold leading-normal'>{name}</h3>
         <p className='px-2 font-medium'>{price}</p>
-        <button class="text-red mt-2 hover:before:bg-redborder-red-500 relative h-[50px] w-full overflow-hidden border-t border-gray-200 bg-white px-3 text-black transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-red-500 before:transition-all before:duration-500 hover:text-white  hover:before:left-0 hover:before:w-full">
+        <button class="text-red mt-2 hover:before:bg-redborder-red-500 relative h-[50px] w-full overflow-hidden border-t border-gray-200 bg-white px-3 text-black transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-customcolor before:transition-all before:duration-500 hover:text-white  hover:before:left-0 hover:before:w-full">
             <span class="relative z-10">Add to Bag</span></button>
 
         </div>
