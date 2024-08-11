f2f161013cff528b9ea9ee2081a77a2458feb191
Even <even.berhane2022@gmail.com>
2024-08-11 14:44:01 +0300

    icons mapping

--- src/constants/index.js
+++ src/constants/index.js
@@ -64,52 +64,12 @@ export const products = [
     },
 ];
 
-export const services = [
-    {
-        imgURL: truckFast,
-        label: "Free shipping",
-        subtext: "Enjoy seamless shopping with our complimentary shipping service."
-    },
-    {
-        imgURL: shieldTick,
-        label: "Secure Payment",
-        subtext: "Experience worry-free transactions with our secure payment options."
-    },
-    {
-        imgURL: support,
-        label: "Love to help you",
-        subtext: "Our dedicated team is here to assist you every step of the way."
-    },
-];
 
-export const reviews = [
-    {
-        imgURL: customer1,
-        customerName: 'Morich Brown',
-        rating: 4.5,
-        feedback: "The attention to detail and the quality of the product exceeded my expectations. Highly recommended!"
-    },
-    {
-        imgURL: customer2,
-        customerName: 'Lota Mongeskar',
-        rating: 4.5,
-        feedback: "The product not only met but exceeded my expectations. I'll definitely be a returning customer!"
-    }
-];
+
 
 
 export const footerLinks = [
-    {
-        title: "Products",
-        links: [
-            { name: "Air Force 1", link: "/" },
-            { name: "Air Max 1", link: "/" },
-            { name: "Air Jordan 1", link: "/" },
-            { name: "Air Force 2", link: "/" },
-            { name: "Nike Waffle Racer", link: "/" },
-            { name: "Nike Cortez", link: "/" },
-        ],
-    },
+   
     {
         title: "Help",
         links: [
@@ -123,14 +83,14 @@ export const footerLinks = [
     {
         title: "Get in touch",
         links: [
-            { name: "customer@nike.com", link: "mailto:customer@nike.com" },
-            { name: "+92554862354", link: "tel:+92554862354" },
+            { name: "customer@wilson.com", link: "mailto:customer@wilson.com" },
+            { name: "+123456789", link: "tel:+12345678" },
         ],
     },
 ];
 
 export const socialMedia = [
-    { src: facebook, alt: "facebook logo" },
-    { src: twitter, alt: "twitter logo" },
-    { src: instagram, alt: "instagram logo" },
+    { src: facebook,href:"https://www.facebook.com/WilsonTennis/",  alt: "facebook logo" },
+    { src: twitter, href:"https://x.com/wilsontennis", alt: "twitter logo" },
+    { src: instagram, href:"https://www.instagram.com/wilsontennis/?hl=en",alt: "instagram logo" },
 ];
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,12 +1,32 @@
+import { headerLogo } from "../assets/images";
+import { socialMedia } from "../constants";
 
 const Footer = () => {
   return (
-<footer className="max-container">
-  <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
+    <footer className="max-container">
+      <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
+        <div className="flex flex-col items-start">
+          <a href="/">
+            <img src={headerLogo} width={150} height={46} alt="Wilson Logo" />
+          </a>
+          <p>Unlock your full potential on the court with Wilson's premium tennis rackets.</p>
+          <div className="flex items-center gap-5 mt-8">
+            {socialMedia.map((icon, index) => (
+              <a
+                key={index}
+                href={icon.href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex justify-center items-center w-12 h-12 rounded-full bg-white"
+              >
+                <img src={icon.src} alt={icon.alt} width={24} height={24} />
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-  </div>
-
-</footer>  )
-}
-
-export default Footer
+export default Footer;
