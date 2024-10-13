a4a54cbe4273e8443190aa4d53fdfb28f727decc
Even <even.berhane2022@gmail.com>
2024-10-13 21:52:39 +0300

    cartcontext

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { CartContext } from '../context/CartContext' // Import the cart context
+import { CartContext } from '../context/CartContext';
 
 const Button = ({ label, iconURL, onClick }) => {
   return (
